2765a065d1765685ca7e41355bd955fda2f41455
Jefek <jeffrey.knowles@gmail.com>
2018-10-29 12:47:24 -0400

    My first Dojo Java file

---
+++ MyJava.java
@@ -0,0 +1,16 @@
+public class MyJava {
+  public static void main(String[] args) {
+    // In the main method, implement the code to print your name, age, and hometown in different lines
+    String Name = "Jeff Knowles Jr";
+    int Age = 35;
+    String Hometown = "Arlington, VA";
+    System.out.println("************************************");
+    System.out.print("Name: ");
+    System.out.println(Name);
+    System.out.print("Age: ");
+    System.out.println(Age);
+    System.out.print("Hometown: ");
+    System.out.println(Hometown);
+    System.out.println("************************************");
+  }
+}
